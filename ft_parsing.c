b155ee512ccdb2317465279da28e3b1fae31ce54
Julia Batoro <juliabatoro@macbook-pro-de-julia.home>
2022-03-13 15:45:05 +0100

    seems ok

--- ft_parsing.c
+++ ft_parsing.c
@@ -5,15 +5,9 @@ void    ft_get_paths(t_pipex *input, char **env)
     int     i;
 
     i = 0;
-   // printf("%s\n", (*input)->env[i]);
     while (!ft_strnstr(input->env[i], "PATH", 4))
-    {
-            i++;
-    }
+        i++;
     input->paths = ft_split(input->env[i] + 5, ':');
-   /* printf("%s\n", (*input)->paths[0]);
-    printf("%s\n", (*input)->paths[1]);
-    printf("%s\n", (*input)->paths[2]); */
 }
 
 char    *ft_check_one_path(t_pipex *input, int index)
@@ -30,27 +24,16 @@ char    *ft_check_one_path(t_pipex *input, int index)
         return (input->cmd);
     //printf("%s\n", input->cmd);
     ft_get_paths(input, input->env);
-  /* printf("%s\n", input->paths[0]);
-    printf("%s\n", input->paths[1]);
-    printf("%s\n", input->paths[2]); */
     //printf("%s\n", input->cmd);
     i = -1;
     while (input->paths[++i])
     {   
-       // printf("%s\n", input->paths[i]);
-        //get one path : on ajoute le / de fin
         end_slash_path = ft_strjoin(input->paths[i], "/");
         //printf("%s\n", end_slash_path);
         //printf("%s\n", input->cmd);
-        // joindre le path avec la commande
-        path = ft_strjoin(end_slash_path, input->cmd); // ATTENTION AU STR_JOIN 
-        //free (end_slash_path); // on ne va plus l'utiliser
-        // car on va utiliser le path avec cmd
-        // et on veut reutiliser la  variable vide 
-        // en boucle, jusqu'a !input->path
+        path = ft_strjoin(end_slash_path, input->cmd);
         printf("%s\n", path);
-        if (!access (path, F_OK)) // F_OK == existence test
-        // on check si le path existe
+        if (!access (path, F_OK)) 
         {
             ft_free(input->paths);
             return (path);
--- pipex.c
+++ pipex.c
@@ -17,7 +17,7 @@ void    ft_init_struct(t_pipex *input, char **argv, char **envp)
     input->env = envp;
 }
 
-void    ft_child_one_process(t_pipex *input, char **argv)
+void    ft_child1_process(t_pipex *input, char **argv)
 {
     int p_id;
 
@@ -27,17 +27,40 @@ void    ft_child_one_process(t_pipex *input, char **argv)
     if (p_id == 0)
     {
         input->cmd1_arg = ft_split(argv[2], ' ');
-        //printf("%s\n", input->cmd1_arg[0]);
         close(input->fd_pipe[0]);
-        //dup2(input->first_fd, 0);
-        //dup2(input->fd_pipe[1], 1);
-       // printf("%s\n", input->cmd1_arg[0]);
-       //printf("OK");
+        dup2(input->first_fd, 0);
+        dup2(input->fd_pipe[1], 1);
+        close(input->fd_pipe[1]);
         if (input->cmd1_arg[0] && ft_check_one_path(input, 1))
         {
             printf("OK");
             execve(ft_check_one_path(input, 1), input->cmd1_arg, input->env);
             ft_free(input->cmd1_arg);
+        }
+        else
+            perror("error");
+    }
+}
+
+void    ft_child2_process(t_pipex *input, char **argv)
+{
+    int p_id;
+
+    p_id = fork();
+    if (p_id == -1)
+        perror("error fork");
+    if (p_id == 0)
+    {
+        input->cmd2_arg = ft_split(argv[3], ' ');
+        close(input->fd_pipe[1]);
+        dup2(input->second_fd, 1);
+        dup2(input->fd_pipe[0], 0);
+        close(input->fd_pipe[0]);
+        if (input->cmd2_arg[0] && ft_check_one_path(input, 2))
+        {
+            printf("OK");
+            execve(ft_check_one_path(input, 2), input->cmd2_arg, input->env);
+            ft_free(input->cmd2_arg);
         }
         else
             perror("error");
@@ -50,19 +73,17 @@ int main(int argc, char **argv, char **envp)
 
     input = (t_pipex *)malloc(sizeof(t_pipex));
     if (argc != 5)
-        return(ft_error("wrong input"));
+        ft_error("Error Input\n./pipex [file1][cmd1][cmd2][file2]");
     else 
     {
         if (pipe(input->fd_pipe) == -1)
             perror("Something went wrong with pipe()");
         ft_init_struct(input, argv, envp);
-        ft_child_one_process(input, argv);
-        // call child 2
-        // close fd_pipe[0]
-        // close fd_pipe[1]
-        // wait child 1
-        // wait child 2
-        //ft_free(input);
+        ft_child1_process(input, argv);
+        ft_child2_process(input, argv);
+        close(input->fd_pipe[0]);
+        close(input->fd_pipe[1]);
+        free(input);
         return (0);
     }       
 }
